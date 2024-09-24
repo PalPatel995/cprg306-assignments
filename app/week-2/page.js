ccf31b258673e2addba004024b905ac40b804edd
PalPatel995 <palpatel20082004@gmail.com>
2024-09-24 14:16:41 -0600

    Student-info.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,6 +2,22 @@ export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () ={
+    <div>
+      <h1>Pal Patel</h1>
+      <p>
+        Check out my GitHub repository: 
+        <Link href="https://github.com/PalPatel995/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+          GitHub Profile
+        </Link>
+      </p>
+    </div>
+};
+
+export default StudentInfo;
       </main>
     );
   }
