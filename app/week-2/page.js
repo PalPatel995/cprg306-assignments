b53d64f02cb8f0bc3c615357bb09249cefe2bc70
PalPatel995 <palpatel20082004@gmail.com>
2024-09-24 14:17:58 -0600

    Update

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -10,7 +10,7 @@ const StudentInfo = () ={
       <h1>Pal Patel</h1>
       <p>
         Check out my GitHub repository: 
-        <Link href="https://github.com/PalPatel995/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+        <Link href="https://github.com/PalPatel995" target="_blank" rel="noopener noreferrer">
           GitHub Profile
         </Link>
       </p>
